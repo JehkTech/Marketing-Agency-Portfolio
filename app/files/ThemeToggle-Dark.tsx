2bcea5f39a3f9fb2cd18764280781aaa4d1fe21e
JehkTech <jehoiachinkatemangwe1@gmail.com>
2026-05-11 01:41:50 +0300

    chore(lightdesign): remove commented code from ThemeToggle documentation

--- app/files/ThemeToggle-Dark.tsx
+++ app/files/ThemeToggle-Dark.tsx
@@ -11,7 +11,6 @@ import { useEffect, useState } from 'react'
  * import ThemeToggle from '@/components/ThemeToggle'
  * 
  * <nav className="flex items-center justify-between">
- *   {/* other nav items */}
  *   <ThemeToggle />
  *   <button>Get a Quote</button>
  * </nav>
